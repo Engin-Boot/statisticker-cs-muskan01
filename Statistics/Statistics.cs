4421710e5ab4b24cb2073738c3835c4dcd7d0eff
muskan01 <35635037+muskan01@users.noreply.github.com>
2020-09-11 00:05:48 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -5,8 +5,26 @@ namespace Statistics
 {
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
+        //public Stats CalculateStatistics(List<float> numbers) {
             //Implement statistics here
-        }
+        //}
+        public Stats<T> CalculateStatistics<T>(List<T> numbers)
+        {
+            Stats<T> ComputedStats = new Stats<T>();
+            if (numbers.Count == 0)
+            {
+                ComputedStats.average = (dynamic)Double.NaN;
+                ComputedStats.min = (dynamic)Double.NaN;
+                ComputedStats.max = (dynamic)Double.NaN;
+            }
+
+            else
+            {
+                ComputedStats.average = ComputedStats.CalculateAverage(numbers);
+                ComputedStats.min = ComputedStats.CalculateMin(numbers);
+                ComputedStats.max = ComputedStats.CalculateMax(numbers);
+                
+            }
+            return ComputedStats;
     }
 }
