b4f38353e0bed18d3f04b99b16c4072d0af57382
muskan01 <35635037+muskan01@users.noreply.github.com>
2020-09-11 00:03:40 +0530

    Create Stats.cs

---
+++ Statistics/Stats.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VitalAssignment
+{
+    class Stats<T>
+    {
+
+        public T average;
+        public T min;
+        public T max;
+
+       private T Add(T a, T b)
+        {
+            dynamic a1 = a;
+            dynamic b1 = b;
+            return a1 + b1;
+        }
+
+        private T Divide(T a, int b)
+        {
+            dynamic a1 = a;
+           
+            return a1 / b;
+        }
+
+        public T CalculateAverage(List<T> numbers)
+        {
+            dynamic sum = numbers[0];
+            int n = numbers.Count;
+            for (int i=1; i < n ; i++)
+            {
+                sum = Add(sum,numbers[i]);
+            }
+          return Divide(sum,n);
+         }
+
+        public T CalculateMin(List<T> numbers)
+        {
+            dynamic minimum = numbers[0];
+            int n = numbers.Count;
+            for (int i = 0; i < n; i++)
+            {
+                dynamic a = numbers[i];
+                if ( a < minimum)
+                {
+                    minimum = a;
+                }
+            }
+            return minimum;
+        }
+
+        public T CalculateMax(List<T> numbers)
+        {
+            dynamic maximum = numbers[0];
+            int n = numbers.Count;
+            for (int i = 0; i < n; i++)
+            {
+                dynamic a = numbers[i];
+                if (a > maximum)
+                {
+                    maximum = a;
+                }
+            }
+            return maximum;
+        }
+    }
+}
